f9f4a2b4a224a207f8ddf8d4df4199648697b904
Sergey Yakovlev <selfuryon@gmail.com>
2022-02-07 18:06:53 +0300

    Update schemadir for gsettings

--- users/syakovlev/sway-desktop/sway.nix
+++ users/syakovlev/sway-desktop/sway.nix
@@ -4,15 +4,16 @@ let
     config="''${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
     if [ ! -f "$config" ]; then exit 1; fi
 
+    gnome_schema_dir="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas"
     gnome_schema="org.gnome.desktop.interface"
     gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
     icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
     cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
     font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
-    gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
-    gsettings set "$gnome_schema" icon-theme "$icon_theme"
-    gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
-    gsettings set "$gnome_schema" font-name "$font_name"
+    gsettings --schemadir $gnome_schema_dir set "$gnome_schema" gtk-theme "$gtk_theme"
+    gsettings --schemadir $gnome_schema_dir set "$gnome_schema" icon-theme "$icon_theme"
+    gsettings --schemadir $gnome_schema_dir set "$gnome_schema" cursor-theme "$cursor_theme"
+    gsettings --schemadir $gnome_schema_dir set "$gnome_schema" font-name "$font_name"
   '';
 in { 
   # ws1: terminal
