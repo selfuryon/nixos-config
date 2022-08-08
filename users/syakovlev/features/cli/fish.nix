b7520b45f46b895e67c5cc24aa32e45a80e1a33d
Sergey Yakovlev <selfuryon@gmail.com>
2022-08-08 16:00:21 +0300

    Configure zellij

--- users/syakovlev/features/cli/fish.nix
+++ users/syakovlev/features/cli/fish.nix
@@ -18,6 +18,11 @@ in {
     };
     interactiveShellInit = ''
       fish_vi_key_bindings
+      if status is-interactive; and set -q SSH_TTY;
+        set ZELLIJ_AUTO_ATTACH true
+        set ZELLIJ_AUTO_EXIT true
+        eval (zellij setup --generate-auto-start fish | string collect)
+      end
     '';
   };
 }
