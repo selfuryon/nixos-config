051124dda9fbbaacc6895afd2f6112e31e5ee7c3
Sergey Yakovlev <selfuryon@gmail.com>
2022-10-22 00:03:47 +0300

    Fix tylze

--- machines/tylze/networking.nix
+++ machines/tylze/networking.nix
@@ -19,11 +19,7 @@
     
     # IPv4 configuration
     interfaces.wlp1s0.ipv4.addresses = [{
-      address = "192.168.0.2";
-      prefixLength = 24;
-    }];
-    interfaces.enp2s0.ipv4.addresses = [{
-      address = "192.168.0.3";
+      address = "192.168.0.10";
       prefixLength = 24;
     }];
     defaultGateway = "192.168.0.1";
