a1f77e9f856b287d67567a98bce4e9c9cfce8577
Sergey Yakovlev <selfuryon@gmail.com>
2023-01-04 23:21:53 +0200

    Update podman config

--- machines/common/global/podman.nix
+++ machines/common/global/podman.nix
@@ -2,8 +2,8 @@
   environment.systemPackages = with pkgs; [ podman-compose ];
   virtualisation.podman = {
     enable = true;
+    extraPackages = [ pkgs.zfs ];
     dockerCompat = true;
-    dockerSocket.enable = true;
     defaultNetwork.dnsname.enable = true;
   };
 }
