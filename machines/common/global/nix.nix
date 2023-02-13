ef3cb354767d7b4bd53960d6a8d0b050493a055c
Sergey Yakovlev <selfuryon@gmail.com>
2023-02-13 18:58:41 +0200

    fix: fix nix config

--- machines/common/global/nix.nix
+++ machines/common/global/nix.nix
@@ -8,13 +8,22 @@
   # Nix
   nix = {
     settings = {
-      substituters = ["https://hyprland.cachix.org"];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       trusted-users = ["root" "@wheel"];
     };
-    extraOptions = "experimental-features = nix-command flakes repl-flake";
+
+    extraOptions = ''
+      experimental-features = nix-command flakes repl-flake
+      keep-outputs = true
+      keep-derivations = true
+    '';
     gc = {
       automatic = true;
       dates = "weekly";
