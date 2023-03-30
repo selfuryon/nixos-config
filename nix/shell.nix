68f380d045e0eb198d6b89d88a56bc922d4a1b04
Sergey Yakovlev <selfuryon@gmail.com>
2023-03-31 00:55:19 +0300

    chore(mission-critical): change shell thing

--- nix/shell.nix
+++ nix/shell.nix
@@ -6,21 +6,18 @@
     inputs',
     ...
   }: let
-    inherit (config.mission-control) installToDevShell;
     inherit (pkgs) mkShellNoCC;
     inherit (inputs'.deploy-rs.packages) deploy-rs;
     inherit (inputs'.ragenix.packages) ragenix;
     inherit (pkgs) statix;
   in {
-    devShells.default = let
-      shell = mkShellNoCC {
-        name = "etherno-iac";
-        packages = [deploy-rs ragenix statix];
-        shellHook = ''
-          ${config.pre-commit.installationScript}
-        '';
-      };
-    in
-      installToDevShell shell;
+    devShells.default = mkShellNoCC {
+      name = "etherno-iac";
+      inputsFrom = [config.mission-control.devShell];
+      packages = [deploy-rs ragenix statix];
+      shellHook = ''
+        ${config.pre-commit.installationScript}
+      '';
+    };
   };
 }
