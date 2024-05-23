06dd2e24ee940f4eb69bf534df361895b7ff4ee8
Sergey Yakovlev <selfuryon@gmail.com>
2024-05-23 04:48:55 +0300

    chore: add rp_filter to tailscale interface

--- nixos/roles/system/optional/tailscale.nix
+++ nixos/roles/system/optional/tailscale.nix
@@ -4,4 +4,7 @@
     checkReversePath = "loose";
     allowedUDPPorts = [41641]; # Facilitate firewall punching
   };
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.tailscale0.rp_filter" = 2;
+  };
 }
