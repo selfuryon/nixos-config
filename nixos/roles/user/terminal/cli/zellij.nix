1ae0fd2a45c761ab1255784403a5234cf4fd9dc4
Sergey Yakovlev <selfuryon@gmail.com>
2024-10-10 01:42:39 +0300

    chore: package update

--- nixos/roles/user/terminal/cli/zellij.nix
+++ nixos/roles/user/terminal/cli/zellij.nix
@@ -5,6 +5,16 @@
     settings = {
       ui.pane_frames.rounded_corners = true;
       # theme = "catppuccin-latte";
+      keybinds = {
+        "shared_except \"locked\"" = {
+          "bind \"Alt f\"" = {
+            "LaunchPlugin \"filepicker\"" = {
+              #floating=true;
+              close_on_selection = true;
+            };
+          };
+        };
+      };
     };
   };
   xdg.configFile."zellij/layouts" = {
