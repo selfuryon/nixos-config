859e35aaebaabcff8e28c9b013e56949aaa62913
Sergey Yakovlev <selfuryon@gmail.com>
2025-01-09 04:15:52 +0200

    chore(waybar): change icon for mail

--- nixos/roles/user/desktop/common/waybar.nix
+++ nixos/roles/user/desktop/common/waybar.nix
@@ -66,7 +66,7 @@ in {
             term = "";
             code = "";
             browser = "";
-            mail = "󰊫";
+            mail = "";
             im = "";
             secret = "";
             default = "";
